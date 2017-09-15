c03fbf8da073edbf6a32ca3a66d043d4182c6b1b
yparedes <yussel_paredes@hotmail.com>
2017-09-15 01:51:18 -0500

    Added authHttp for making authenticated requests

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,6 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { AuthConfig, AuthHttp } from 'angular2-jwt';
+import { Http, RequestOptions } from '@angular/http';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig({
+    globalHeaders: [{'Content-Type': 'application/json'}]
+  }), http, options);
+}
 
 @NgModule({
   declarations: [
@@ -13,6 +21,13 @@ import { AuthRoutingModule } from './auth-routing.module';
   imports: [
     FormsModule,
     AuthRoutingModule
+  ],
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: authHttpServiceFactory,
+      deps: [Http, RequestOptions]
+    }
   ]
 })
 export class AuthModule {}

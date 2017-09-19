63a737609824243f968651b7b68d95be1f27d155
Yussel Paredes <yussel_paredes@hotmail.com>
2017-09-19 18:01:16 -0500

    Added rxjs map, switchMap, catch operators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import {SharedModule} from './shared/shared.module';
@@ -30,6 +33,7 @@ import { GeocoderService } from './map/geocoder.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+
     SharedModule,
     AuthModule,
     DirectoryModule,
